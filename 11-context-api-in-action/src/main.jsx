817fe1e90a39007060d28e9693219fc6b299a91b
bharatXjaidiya <jaidiyabharat16@gmail.com>
2026-03-01 12:07:45 +0530

    hash-router-added

--- 11-context-api-in-action/src/main.jsx
+++ 11-context-api-in-action/src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 import ProductContext from './context/ProductContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <ProductContext>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ProductContext>
 )
